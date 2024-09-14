d0e43338c08c19ddb3ea953492fa516459d4b1ff
Arfa Ahsan <150623089+Arfa-Ahsan@users.noreply.github.com>
2024-09-14 12:43:47 -0700

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -10,18 +10,13 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords  
 
 
-st.markdown(
-    """
+hide = """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
     </style>
-    """,
-    unsafe_allow_html=True
-)
+"""
+st.markdown(hide, unsafe_allow_html=True)
 # Load the saved models 
 model_paths = {
     'Logistic Regression': 'Models/logistic_regression_model.pkl',
