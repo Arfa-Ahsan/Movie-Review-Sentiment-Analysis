7d6c3c2c36418b23b06f4302679249b6dc4fea91
Arfa Ahsan <150623089+Arfa-Ahsan@users.noreply.github.com>
2024-09-14 10:04:57 -0700

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -127,7 +127,7 @@ if st.button('Predict Sentiment') and text:
             sentiment_values = [probability[0], probability[1]]
             
             # Bar chart for sentiment distribution
-            plt.figure(figsize=(3, 3))
+           plt.figure(figsize=(3, 2))
             plt.bar(sentiment_labels, sentiment_values, color=['#d031e8','#f5c7fc'])
             plt.ylabel('Probability')
             plt.title(f'Sentiment Probability Distribution')
