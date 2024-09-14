d199c40cb6ea2b59a21fbd7ad631d9c769ac7ac5
Arfa Ahsan <150623089+Arfa-Ahsan@users.noreply.github.com>
2024-09-14 12:42:44 -0700

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -13,9 +13,11 @@ from nltk.corpus import stopwords
 st.markdown(
     """
     <style>
-    #MainMenu {
-      visibility: hidden;
-   }
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
     </style>
     """,
     unsafe_allow_html=True
